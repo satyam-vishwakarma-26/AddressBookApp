776cd56dc614833d3d12f80371ddeca55b4fc3c7
Satyam Vishwakarma <153850625+satyam-vishwakarma-26@users.noreply.github.com>
2026-03-08 17:59:27 +0530

    [Satyam] : Fix verbose output for extracted Maven directory

--- mvnw.cmd
+++ mvnw.cmd
@@ -173,7 +173,7 @@ if (!$actualDistributionDir) {
   Write-Error "Could not find Maven distribution directory in extracted archive"
 }
 
-Write-Verbose "Found extracted Maven distribution directory: $actualDistributionDir"
+ Write-Verbose "Found extracted Maven distribution directory: $actualDistributionDir"
 Rename-Item -Path "$TMP_DOWNLOAD_DIR/$actualDistributionDir" -NewName $MAVEN_HOME_NAME | Out-Null
 try {
   Move-Item -Path "$TMP_DOWNLOAD_DIR/$MAVEN_HOME_NAME" -Destination $MAVEN_HOME_PARENT | Out-Null
