f62a62ec60136c5e8b8d14256339f77f14d6ec42
satyam-vishwakarma-26 <satyamsharma000000s@gmail.com>
2026-03-08 14:06:18 +0530

    Initial commit - AddressBook Spring Boot project

---
+++ src/main/java/com/satyam/addressbooksystem/AddressBookAppMain.java
@@ -0,0 +1,14 @@
+package com.satyam.addressbooksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		//SpringApplication.run(AddressBookAppMain.class, args);
+	}
+
+}
---
+++ src/test/java/com/satyam/addressbooksystem/AddressBookAppMainTests.java
@@ -0,0 +1,13 @@
+package com.satyam.addressbooksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
